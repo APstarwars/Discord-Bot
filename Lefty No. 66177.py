27ffee473c547778bfe072f14059ab52389d7396
APstarwars <32665017+APstarwars@users.noreply.github.com>
2019-09-06 16:04:48 -0700

    Add >pick and >8ball
    Lefty can now pick between options and answer questions.

--- Lefty No. 66177.py
+++ Lefty No. 66177.py
@@ -1,29 +1,48 @@
 import discord
 from discord.ext import commands
+import random
 
 TOKEN = open("token.txt", "r").read()
-bot = commands.Bot(command_prefix='>')
 
-@bot.event
-async def on_ready():
-    print("Lefty No. 66177 Online")
+class LeftyBot(discord.Client):
+    async def on_ready(self):
+        print("{0.name} Online".format(self.user))
 
-@bot.event
-async def on_message(message):
-    print(f"{message.author.name}: {message.content}")
-    if message.author == bot.user:
-        return
-    prof = ""
-    if str(message.author) == "Anderson Programmer#2568":
-        prof = " Professor"
-    if "hello lefty" in message.content.lower():
-        await message.channel.send('Hello' + prof + ', how may I help?')
-    if ">echo" in message.content:
-        x = message.content.split(" ", 1)
-        await message.channel.send(x[1])
+    async def on_message(self, message):
+        print("{0.author.name}: {0.content}".format(message))
+        if message.author == self.user:
+            return
+        prof = ""
+        if str(message.author) == "Anderson Programmer#2568":
+            prof = " Professor"
+        if ">echo" in message.content:
+            x = message.content.split(" ", 1)
+            await message.channel.send(x[1])
+        elif ">pick" in message.content:
+            x = message.content.split(" ", 1)
+            y = x[1].split("|")
+            await message.channel.send(y[random.randint(0, len(y)-1)])
+        elif ">8ball" in message.content:
+            x = random.randint(0, 7)
+            if x==0:
+                msg = "It is not likely"
+            elif x==1:
+                msg = "Perhaps, if you try hard enough"
+            elif x==2:
+                msg = "My calculations returned True"
+            elif x==3:
+                msg = "I wouldn't count on it"
+            elif x==4:
+                msg = "Definitely, with 99.89 percent certainty"
+            elif x==5:
+                msg = "Impossible with our current state of technology"
+            elif x==6:
+                msg = "I must apologise, I cannot say"
+            elif x==7:
+                msg = "That is information I cannot disclose"
+            await message.channel.send(':8ball:| ' + msg + ',' + prof + ' **{0.author.name}**'.format(message))
+        elif "hello lefty" in message.content.lower():
+            await message.channel.send('Hello' + prof + ', how may I help?')
 
-@bot.command()
-async def echo(ctx):
-    print("help plz")
-
+bot = LeftyBot()
 bot.run(TOKEN)
